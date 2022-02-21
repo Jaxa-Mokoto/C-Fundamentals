fd8941d45fb23dd62f70dcd4656f774c44435677
Jaxa Mokoto <Jaxa.Mokoto@arcinfra.com>
2022-02-21 21:51:53 +0800

    added comments to define properties

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+//For more information on the System namespace: https://docs.microsoft.com/en-us/dotnet/api/system?view=net-6.0
 
 namespace HelloWorld
 {
     internal class Program
-    {
-        static void Main(string[] args)
+    { //Curly braces define the code into 'blocks'
+        static void Main(string[] args) 
+            //parenthesis are called when defining methods. They can be given parameters.
+            //For the Main() method, the parameter defined is 'args' 
         {
-            Console.WriteLine("Hello World - this is my first C# application.");
+            //Console.WriteLine() is a method defined in the System namespace as defined above in 'using'
+            //For the WriteLine() method, the parameters defined are of the String datatype
+            Console.WriteLine("Hello World");
         }
     }
 }
