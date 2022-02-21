0bb9c41a5f68971d10e95843a0c7e3e6ef353ae4
Jaxa Mokoto <Jaxa.Mokoto@arcinfra.com>
2022-02-21 22:06:20 +0800

    Initial Eliza application

---
+++ Eliza/Eliza/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ElizaIsSilly
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Call the Intro() method in the Doctor class to initialize the program.
+
+            Console.WriteLine(Doctor.Intro());
+        }
+    }
+}
