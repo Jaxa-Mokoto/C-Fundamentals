0615c18b50424c6e79ba8adfa14c3e6d80cb8e82
Jaxa Mokoto <Jaxa.Mokoto@arcinfra.com>
2022-02-27 19:05:08 +0800

    adding examples on formatting strings and numerical values

--- StringFormatting/Program.cs
+++ StringFormatting/Program.cs
@@ -18,9 +18,19 @@ namespace StringFormatting
             string columnTwoHeading = "Quantity";
             string columnThreeHeading = "Price";
 
-            Console.WriteLine($"{columnOneHeading, -12} {columnTwoHeading, 8} {columnThreeHeading, 8}");
-            Console.WriteLine($"{apples, -12} {appleQuantity, -8} {applePrice, 6} cents");
-            Console.WriteLine($"{oranges, -12} {orangeQuantity, -8} {orangePrice, 6} cents");
+            Console.WriteLine($"{columnOneHeading,-12} {columnTwoHeading,8} {columnThreeHeading,9}");
+            Console.WriteLine($"{apples,-12} {appleQuantity,-8} {applePrice / 100.0,9:C}"); // :C displays the integer in a currency. For Linux machines, the default currency is dollars  ($).
+            Console.WriteLine($"{oranges,-12} {orangeQuantity,-8} {orangePrice / 100.0,9:C}");
+
+            //Experimenting with PI's multiple decimal places (using the F format string followed by demial places desired):
+            //(Resource) Microsoft format strings documentation: https://docs.microsoft.com/en-us/dotnet/standard/base-types/standard-numeric-format-strings
+
+            Console.WriteLine($"PI is {Math.PI}");
+            Console.WriteLine($"PI is {Math.PI:F3}");
+            Console.WriteLine($"PI is {Math.PI:F0}");
+            Console.WriteLine($"PI is {Math.PI:F12}");
+            Console.WriteLine($"PI is {Math.PI:f99}");
+
             Console.ReadLine();
         }
     }
